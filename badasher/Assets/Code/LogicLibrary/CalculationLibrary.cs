a056f47837cf759b7e451d6fba36fce553b60d44
Elda <sakari.helokunnas@gmail.com>
2017-07-15 13:38:00 +0300

    Slowdown calculation method

--- badasher/Assets/Code/LogicLibrary/CalculationLibrary.cs
+++ badasher/Assets/Code/LogicLibrary/CalculationLibrary.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CalculationLibrary : MonoBehaviour {
+public static class CalculationLibrary {
 	// Holds methods for jump vs dash distance, dash slowdown vs dash distance
 
-	public int CalculateDashSlowdown(float dashDistance){
-		return 0;
+	public static float CalculateDashSlowdown(float dashDistanceRemaining){
+		float slowdown;
+		float difference = PlayerConstants.DASH_DISTANCE - dashDistanceRemaining;
+		if (difference <= PlayerConstants.DASH_SLOWDOWN_PERFECT_PERCENTAGE * PlayerConstants.DASH_DISTANCE) {
+			return 0; // no slowdown
+		} else if (difference >= PlayerConstants.DASH_SLOWDOWN_END_PERCENTEGE * PlayerConstants.DASH_DISTANCE) {
+			return difference*PlayerConstants.DASH_SLOWDOWN_END_MODIFIER;
+		} else {
+			return difference;
+		}
+		// slowdown is normalized difference?
 	}
 
-	public int CalculateDashJump(float dashDistance){
-		return 0;
+	public static Vector3 CalculateDashJumpDir(float dashDistanceRemaining){
+		return new Vector3();
+	}
+
+	public static float CalculateDashJumpPower(float dashDistanceRemaining){
+		return 10f;
 	}
 }
