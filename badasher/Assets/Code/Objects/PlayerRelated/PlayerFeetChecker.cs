57623a0d30a783bbeb8b1a7293deaf70eb74e412
Elda <sakari.helokunnas@gmail.com>
2017-07-16 03:40:20 +0300

    Player will now drop down if left without platform

---
+++ badasher/Assets/Code/Objects/PlayerRelated/PlayerFeetChecker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFeetChecker : MonoBehaviour {
+
+
+	Player player;
+	Animator playerAnimator;
+
+	public void Start(){
+		player = gameObject.GetComponentInParent<Player> ();
+		playerAnimator = gameObject.GetComponentInParent<Animator> ();
+	}
+
+	public void OnTriggerExit2D (Collider2D other){
+		if (other.CompareTag ("Ground")) {
+			player.SetAirState (Player.AirState.air);
+			playerAnimator.SetBool ("InAir", true);
+		}
+	}
+}
