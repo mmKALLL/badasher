8317ab06637021d7efa9f9ab66044547423f1925
mmKALLL <esakoskin2@gmail.com>
2017-07-16 06:23:48 +0300

    Fixed background rendering, added HSV transformations to designate stage transitions.

--- badasher/Assets/Code/_Gamemanager/GameLauncher.cs
+++ badasher/Assets/Code/_Gamemanager/GameLauncher.cs
@@ -8,7 +8,7 @@ public class GameLauncher : MonoBehaviour {
 	public GameObject groundEnemy;
 	public GameObject airEnemy;
 	public GameObject mine;
-	public GameObject player;
+	public GameObject playerPrefab;
 	public GameObject floor;
 	public GameObject ramp;
 	public GameObject background;
@@ -22,19 +22,20 @@ public class GameLauncher : MonoBehaviour {
 
 
 	protected internal class StageGeneratorConstants {
-		// The game is divided into stages, which are divided into screens, which essentially represent 50 unit blocks.
-		// Each screen should be the size of one BG image, which are then looped to fill the stage.
-		// Each stage may have differing properties.
 
-		public const int GAME_LENGTH = 10; // Length in stages
-		public const int STAGE_DEFAULT_LENGTH = 40; // Length in stages
-		public int[] STAGE_LENGTHS = new int[GAME_LENGTH];
-		private int GAME_LENGTH_SCREENS = 0;
+		public int GAME_LENGTH; // Length in stages
+		public int STAGE_DEFAULT_LENGTH; // Length in stages
+		public int[] STAGE_LENGTHS;
+		private int GAME_LENGTH_SCREENS;
 		public int GetGameLengthInScreens() {
 			return GAME_LENGTH_SCREENS;
 		}
 
 		public StageGeneratorConstants() {
+			GAME_LENGTH = 10;
+			STAGE_DEFAULT_LENGTH = 40;
+			GAME_LENGTH_SCREENS = 0;
+			STAGE_LENGTHS = new int[GAME_LENGTH];
 			for (int i = 0; i < STAGE_LENGTHS.Length; i++) {
 				STAGE_LENGTHS[i] = STAGE_DEFAULT_LENGTH;		// It is expected below that each stage is equal in length.
 				GAME_LENGTH_SCREENS += STAGE_DEFAULT_LENGTH;
@@ -52,13 +53,29 @@ public class GameLauncher : MonoBehaviour {
 			z * gameObject.transform.localScale.z / gameObject.GetComponent<Renderer>().bounds.size.z);
 	}
 
+	/**
+	 * GenerateStage - a random stage generator.
+	 * 
+	 * Starts with an initial fixed stage part, and creates floorings after that based on jump difficulty, which increases over time.
+	 * 
+	 * The game is divided into stages, which are divided into screens, which essentially represent 50 unit blocks.
+	 * Each screen should be the size of one BG image, which are then looped to fill the stage.
+	 * Each stage may have differing properties.
+	 * 
+	 * Viewport size assumed to be around 50-70 units wide and 30 units tall. y = 0 is the middle part of stage height, and the game
+	 * starts from x = 0. The y value can be negative, but player should be killed at around y = -20. Camera should never reveal area
+	 * below or above the background, which has y values in the range [-25, 25].
+	 * 
+	 * 
+	 * 
+	 */
 	public void GenerateStage(){
-
-		// Ingame screen size - UI exlusive - expected to be around 50x20 units
+		
 		// Initialize constants for stage generation.
 		StageGeneratorConstants sg = new StageGeneratorConstants ();
 
 		// Arrays for holding the objects, sorted by initial X coordinate.
+		GameObject player;
 		List<GameObject> floors = new List<GameObject>();
 		List<GameObject> ramps = new List<GameObject>();
 		List<GameObject> airEnemies = new List<GameObject>();
@@ -70,13 +87,26 @@ public class GameLauncher : MonoBehaviour {
 		//GameObject instantiatedObject = Instantiate(groundEnemy, new Vector3(1,1,1), Quaternion.identity);
 		//instantiatedObject.transform.localScale = new Vector3 (5, 1, 1);
 
-
-
+		
+		// Generate backgrounds
 		int totalScreens = sg.GetGameLengthInScreens ();
 		for (int i = 0; i < totalScreens; i++) {
-			backgrounds.Add (Instantiate(background, new Vector3(80 * i - 80, 0, 20), Quaternion.identity));
-			backgrounds[i].transform.localScale = normalizeToSize(backgrounds[i], 80, 40, 0);
+			backgrounds.Add (Instantiate(background, new Vector3(100 * i - 100, 0, 20), Quaternion.identity));
+			if (i > sg.STAGE_DEFAULT_LENGTH) {
+				backgrounds [i].GetComponent<Renderer> ().material.color = Color.HSVToRGB (
+					(i / sg.STAGE_DEFAULT_LENGTH) * 1.0f / sg.GAME_LENGTH,
+					0.4f,
+					1.0f);
+			}
+			backgrounds[i].transform.localScale = normalizeToSize(backgrounds[i], 100, 50, 0);
 		}
+
+		// Initial start area
+		
+
+		// Generate floors
+		int x = 0;
+
 	}
 
 }
